61dbccf10b76f8c6ddcafcde953346ea7a19b173
Harshmishra1995 <harsh@logicloop.io>
2022-08-09 09:58:05 +0530

    Added controls in component

---
+++ ipodmini/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Controls from './components/Controls';
+function App() {
+  return (
+    <div className="App">
+      <Controls/>
+    </div>
+  );
+}
+
+export default App;
